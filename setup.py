8db1755389830a1496ccc31cbef7cc3e5e9fd0fa
Benjamin Ward <ward.programm3r@gmail.com>
2017-11-24 13:07:55 -0500

    Bump to 1.2.1 to fix PyPI upload issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='tbapiv3',
-    version='1.2.0',
+    version='1.2.1',
     packages=['tbapiv3'],
     url='https://github.com/WardBenjamin/TBAPIv3',
     license='MIT',
